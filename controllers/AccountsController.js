7c280caeb135e4cb49deb379670cafd3e27dbb2a
Sofia Galkina <gal18024@byui.edu>
2024-03-23 19:14:25 -0600

    model schema, route and connection added for user accounts

---
+++ controllers/AccountsController.js
@@ -0,0 +1,19 @@
+const supplies = require('../models/AccountsModel');
+
+//Get all accounts
+const getAllAccounts = async (req, res) => {
+    try {
+        console.log("Getting all user account information");
+        const accountsList = await accounts.find();
+        console.log(accountsList);
+        res.json(accountsListList);
+    }
+    catch (err) {
+        console.log(err);
+        res.json({message: err});
+    }
+};
+
+module.exports = {
+    getAllAccounts
+};
